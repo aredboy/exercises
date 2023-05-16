c652461b6a3e624951b3963b983c63af83cae3f1
aredboy <104846492+aredboy@users.noreply.github.com>
2023-05-16 16:56:12 -0300

    Ejercicio con respuestas
    Hice los que faltaban. No se si esta bien subirlo directamente aca al main branch o es mejor usar PR de otra manera.

--- Ejercicio DJ/script.js
+++ Ejercicio DJ/script.js
@@ -1,130 +1,204 @@
 // Variables
 
+let name = "Nathan";
 
-// Create a variable called 'name' -which cannot be reassigned- and give it a value of "nathan".
-let name = "nathan";
-// La asignacion de la variable esta bien, pero dice "cannot be reassigned". Para eso podes usar `const`:
-// const name = "nathan";
-
-// Create a variable called 'age' -which can be reassigned- and give it a value of 25.
 let age = 25;
-// OK
 
-// Create a variable called 'isMexican' -which cannot be reassigned- and give it a value of true.
+
 const isMexican = true;
-// OK
 
-// Create a variable called 'hobbies' -which cannot be reassigned- and give it a value of an array with the following values:
-// - "djing"
-// - "sex"
-// - "coding"
 const hobbies = ["djing", "sex", "coding"];
-// OK
 
-// Create an object called 'person' with the following properties:
-// - name
-// - age
-// - isMexican
-// - hobbies
-// When creating the object, use the variables you created above. (Do not re-type the values)
 const person = {
     name,
     age,
     isMexican,
     hobbies
 };
-// Perfecto!
 
+console.log(sayHello())
 
-
-console.log(typeof sayHello())
-// Aca no entendi qué quiciste hacer. Si queres imprimir a la consola el resultado de sayHello
-// no deberias usar "typeof", sino simplemente llamar a la funcion:
-// console.log(sayHello())
 
 // Functions
 
-// Create a function called 'sayHello' that takes a parameter called 'person' and returns the following string:
-// "Hello, my name is " + name + " and I am " + age + " years old."
-// When calling the function in the next step, pass in the 'person' object you created above, and use its properties to build the string.
-function sayHello() {
+function sayHello(_person) {
     
     return [
         "Hello, my name is " + name + " and I am " + age + " years old."
     ];
 }
-// La definicion de la funcion está bien, pero te falto el "takes a parameter called 'person'".
-// Fijate que la funcion no acepta ningun parametro (tenes que incluirlo dentro del parentesis).
-// Asi:
-// ```
-// function sayHello(person) {
-//     // el resto de la funcion
-// }
-// ```
-// Como no le pasaste person como parametro a la funcion, las variables "name" y "age" que estas usando dentro de la funcion
-// no corresponden al objeto person, sino a las variables que creaste afuera, que tienen el mismo nombre.
-// La idea es que vos le puedas pasar a `sayHello` cualquier objeto `person` y que te imprima su name y su age.
-// La funcion deberia ser asi:
-// function sayHello(person) {
-//      return "Hello, my name is " + person.name + " and I am " + person.age + " years old."
-// }
-// Y la idea es que la llames asi:
-// const rojo = {
-//     name: "German",
-//     age: 85,
-//     isMexican: false,
-//     hobbies: ["drogas", "liberalismo"]
-// }
-// console.log(sayHello(rojo))
-//  deberia imprimir a la consola: "Hello, my name is German and I am 85 years old."
-// Pero tambien podes usar la misma funcion con otro objeto:
-// const djere = {
-//     name: "JP",
-//     age: 34,
-//     isMexican: false,
-//     hobbies: ["fascismo"]
-// }
-// console.log(sayHello(djere))
-//  deberia imprimir a la consola: "Hello, my name is JP and I am 34 years old."
-// 
-// Otra cosa: la funcion tiene que devolver directamente un string, no un array con un string adentro.
-// Fijate en tu definicion de la funcion
 
 
-// No entendi que quisiste hacer aca
-if (typeof sayHello === 'object') {
-    sayHello(person) = sayHello.toString();
-};
 
-// Call the function you created above and store the result in a variable called 'greeting'.
 let greeting = sayHello(person).toString()
-// El llamado de la funcion y la asignacion a greeting esta bien, pero no te hubise funcionado pq cuando definiste
-// sayHello arriba, no toma ningun input. Por otro lado, el toString() no es necesario, la funcion deberia devolver
-// directamente un string.
-// let greeting = sayHello(person);
 
 console.log(greeting)
 
-// Now create another variable called parsedGreeting by splitting the greeting variable on each space. (Remember to use split() method)
 let parsedGreeting = greeting.split(" ")
-// OK!
 
-console.log(typeof parsedGreeting)
 
-// Aca la idea estaba bien: creas una funcion llamada uppercase, y despues se la pasas como "callback" a map.
-// Pero esta mal planteada la funcion. La sintaxis esta mal (esto no compila) y fijate que por otro lado, tu funcion
-// no devuelve nada.
-function uppercase(greeting) {
-    {greeting.toUpperCase}parsedGreeting
+console.log(parsedGreeting)
+
+
+// Now create another variable called uppercaseGreeting by mapping over the parsedGreeting array 
+// and converting each word to uppercase. (Remember to use map() method)
+
+function uppercase(parsedGreeting) {
+    return parsedGreeting.toUpperCase()
 }
-// Defino la funcion, que toma como argumento "word", que va a ser cada elemento dentro del array 'parsedGreeting'
-// function uppercase(word) {
-//     // Devuelvo esa palabra, pero convertida a uppercase
-//     return word.toUpperCase();
-// }
 
-// Esto esta bien, pero no es necesario el segundo argumento cdo usas map
-let uppercaseGreeting = parsedGreeting.map(uppercase,this);
-// let uppercaseGreeting = parsedGreeting.map(uppercase);
+let uppercaseGreeting = parsedGreeting.map(uppercase);
 
 console.log(uppercaseGreeting)
+
+// Now create another variable called finalGreeting by joining the uppercaseGreeting array back 
+// into a string. (Remember to use join() method)
+
+let finalGreeting = uppercaseGreeting.join(" ")
+
+console.log(finalGreeting)
+
+
+// Loop through the properties of the person object and console.log each property name and its value.
+// The output should look like this:
+// name: nathan
+// age: 25
+// isMexican: true
+// hobbies: ["djing", ...etc]
+// Hint: You can use a for...of loop to loop through an object's properties. (It's called for...of loop in the docs)
+          // Or you can use Object.keys() to get an array of the object's property names, and then loop through that array.
+          // A final option is to use Object.entries()
+          // Check the documentation to understand how these methods work.
+
+// Answer
+// One way would be..
+// for (prop in person) {
+//     console.log(`${prop}:${person[prop]}`)
+// }
+
+// Or
+// const props = Object.keys(person)
+// console.log(props)
+// props.forEach((key)=>{
+//     console.log(`${key}:${person[key]}`)
+// })
+
+//Or
+const props = Object.entries(person).forEach(([key,value])=> {
+    console.log(`${key}:${value}`)
+})
+
+
+// Write a for loop that iterates from 25 to 100. For each iteration, it will check if the current 
+// number is even or odd, and display a message in the console.
+
+let num = 24
+let one = 1
+
+while (num<100) {
+    num += one
+    // console.log(num)
+    if (num % 2 == 0) {
+        console.log('number is even')
+    }
+    else {console.log('number is odd')}
+}
+
+
+// Create an object called squareRoots.
+// Write a for loop that iterates from 5000 to -5000. For each number, it will create a property 
+// on that object with that number, and its value should be the square root of that number.
+// For example, squareRoots[25] should equal 5.
+
+const squareRoots = {}
+
+let numb=5000
+let res = Math.sqrt(numb);
+
+for (numb;numb>-5000;numb--) {
+    squareRoots[numb] = res;
+}
+
+console.log(squareRoots)
+
+// No pude lograr que itere en el orden que quiero, el objeto empieza de 0 a 5000 y cuando llega a 5000
+// empieza con el -1 hasta el -5000. 
+
+// Create a function called 'add' that takes two parameters and returns the sum of those two numbers.
+
+
+// One way could be:
+// let params = [1,2];
+// const add = params.reduce((accumulator,currentValue) => {
+//     return accumulator + currentValue
+// },0))
+// console.log(add()
+
+
+// Another:
+// function add() {
+//     return Array.prototype.reduce.call(arguments, function(a,b) {
+//         return a+b;
+//     }, 0)
+// }
+// console.log(add(1,2))
+
+
+// Another:
+function add() {
+    let sum = 0;
+    for(let i=0;i<arguments.length;i++){
+        sum += arguments[i];
+    }
+    return sum;
+}
+console.log(add(1,2))
+
+// Create an ARROW function called 'subtract' that takes two parameters and returns the difference 
+// of those two numbers. (check the documentation for arrow functions)
+
+const subtract = (a,b) => a-b;
+
+console.log(subtract(3,2))
+
+// Create a variable called allNumbers that is an array of numbers from 1 to 100.
+// Using the filter() method of arrays, create a new array called evenNumbers that contains only 
+// the even numbers from the allNumbers array.
+
+// one way..
+// let allNumbers = {}
+// let i = 1 
+
+// allNumbers[Symbol.iterator] = function*(i) {
+//         for(i; i<=100; i+=1) {
+//             yield i;
+//         }
+//     }
+
+// console.log(typeof[...allNumbers])
+
+// another..
+// let all = [];
+
+// for (var i = 1; i <= 100; i++) {
+//     all.push(i)
+// }
+
+// let allNumbers = Array.from(all)
+
+// another..
+
+let allNumbers = [...Array(100).keys()]
+
+console.log(allNumbers)
+
+// More advanced stuff:
+// Create a function called 'addo' that takes an indefinite number of parameters and returns the sum of all of them.
+
+function addo(...args){
+    return args.reduce(function (a, b){
+        return a + b;
+    })
+}
+
+console.log(addo(4,8,9))
